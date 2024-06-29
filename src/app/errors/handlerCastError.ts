8691bc885ff04a3d51ab7c63a6d84b1b64d2f069
tahsi <tahsin16@cse.pstu.ac.bd>
2024-06-30 02:25:52 +0600

    run some script

--- src/app/errors/handlerCastError.ts
+++ src/app/errors/handlerCastError.ts
@@ -1,9 +1,7 @@
 import mongoose from 'mongoose';
 import { TErrorMessage, TErrorResponse } from '../interfaces/error';
 
-const handleCastError = (
-  err: mongoose.Error.CastError,
-): TErrorResponse => {
+const handleCastError = (err: mongoose.Error.CastError): TErrorResponse => {
   const errorMessages: TErrorMessage = [
     {
       path: err?.path,
--- src/app/modules/Services/services.route.ts
+++ src/app/modules/Services/services.route.ts
@@ -7,7 +7,6 @@ import { SlotControllers } from '../Slot/slot.controller';
 import auth from '../../middlewares/auth';
 import { USER_ROLE } from '../User/user.constant';
 
-
 const router = express.Router();
 
 router.post(
